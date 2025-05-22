4adf47798dbaf779eec864678ed02a9e5461e595
Eduardo Antunes <eduardoantune12@gmail.com>
2025-05-22 15:17:08 -0300

    Atividade Type, função saudação e função multiplicar.

---
+++ homework.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicar(num1, num2) {
+    return num1 * num2;
+}
+function dizOi(nome) {
+    return `Olá, ${nome} seja bem vindo!`;
+}
+console.log(multiplicar(7, 8));
+console.log(dizOi("Eduardo"));
---
+++ homework.ts
@@ -0,0 +1,10 @@
+function multiplicar(num1: number, num2: number): number {
+    return num1 * num2;
+}
+
+function dizOi(nome: string): string {
+    return `Olá, ${nome} seja bem vindo!`;
+}
+
+console.log(multiplicar(7, 8));
+console.log(dizOi("Eduardo"));
